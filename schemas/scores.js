737472d096e4ee5b1d2cdc97a33d83c579bc2f55
huitopia <huitopia27@gmail.com>
2022-01-03 21:18:25 +0900

    socre db delete

--- schemas/scores.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const ScoreSchema = new mongoose.Schema({
-  avgIdx: Number,
-  sleepAvg: Number,
-  period: String,
-  userIdx: Number
-})
-
-module.exports = mongoose.model('Score', ScoreSchema)
